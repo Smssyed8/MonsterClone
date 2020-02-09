5b08d2c76b56f98a4595daf8e5ba3576a15fb7ea
syed <s.muazzam.siddiqhi@gmail.com>
2020-02-09 16:03:41 +0530

    commiting my poc project

---
+++ LoginRegister/loginRegisterUI/src/test/java/com/naukriMonsterRep/login/com/loginRegister/LoginRegisterApplicationTests.java
@@ -0,0 +1,11 @@
+package com.naukriMonsterRep.login.com.loginRegister;
+/*
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * //@SpringBootTest class LoginRegisterApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
